4665a6a24b143a8881d2ec5eb64000f1f645054b
kcn2900 <nguyenkevin2900@gmail.com>
2024-05-31 15:37:50 -0500

    Start of Project

---
+++ script.js
@@ -0,0 +1,46 @@
+function add(a, b) {
+    function add(a, b) {
+        const argArr = Array.from(arguments);
+        // console.log(argArr)
+        return argArr.reduce((total, curr) => total + curr, 0);
+    }
+}
+
+function subtract(a ,b) {
+    const argArr = Array.from(arguments);
+    // console.log(argArr)
+    return argArr.reduce((total, curr) => total - curr, 0);
+}
+
+function multiply(a, b) {
+    const argArr = Array.from(arguments);
+    // console.log(argArr)
+    return argArr.reduce((total, curr) => total * curr, 0);
+}
+
+function divide(a, b) {
+        // const argArr = Array.from(arguments);
+        // // console.log(argArr)
+        // return argArr.reduce((total, curr) => total / curr, 1);
+        return a / b;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            add(num1, num2);
+
+    }
+}
